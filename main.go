4a759996f4d2ba9c111c8a62c5c8eadde3aa6b79
Lucas Hild <contact@lucas-hild.de>
2020-03-14 14:03:48 +0100

    Request information about video from ARTE

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
 
 	"github.com/gorilla/websocket"
 )
@@ -17,6 +20,20 @@ type Message struct {
 	Text     string `json:"text"`
 	ClientID string `json:"clientId"`
 	Date     int    `json:"date"`
+}
+
+type MessageWithVideoDetails struct {
+	Type         string       `json:"type"`
+	VideoDetails VideoDetails `json:"videoDetails"`
+	ClientID     string       `json:"clientId"`
+	Date         int          `json:"date"`
+}
+
+type VideoDetails struct {
+	Title       map[string]string `json:"title"`
+	Description map[string]string `json:"description"`
+	URL         map[string]string `json:"url"`
+	Languages   []string          `json:"languages"`
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -51,15 +68,79 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 func handleMessages() {
 	for {
 		msg := <-broadcast
-		for client := range clients {
-			err := client.WriteJSON(msg)
+
+		switch msg.Type {
+		case "requestVideo":
+			details, err := getVideoDetailsFromArte(msg.Text)
 			if err != nil {
-				log.Println(err)
-				client.Close()
-				delete(clients, client)
+				fmt.Println("Error:", err)
 			}
+			sendMessageToAllClients(MessageWithVideoDetails{
+				Type:         "setVideoDetails",
+				VideoDetails: details,
+				ClientID:     "server",
+				Date:         0, // TODO
+			})
+			fmt.Println(details)
+		default:
+			sendMessageToAllClients(msg)
 		}
 	}
+}
+
+func sendMessageToAllClients(msg interface{}) {
+	for client := range clients {
+		err := client.WriteJSON(msg)
+		if err != nil {
+			log.Println(err)
+			client.Close()
+			delete(clients, client)
+		}
+	}
+}
+
+func getVideoDetailsFromArte(url string) (details VideoDetails, err error) {
+	details.Title = make(map[string]string)
+	details.Description = make(map[string]string)
+	details.URL = make(map[string]string)
+
+	r, _ := regexp.Compile(`\d+-\d+-A`)
+	videoID := r.FindString(url)
+
+	if videoID == "" {
+		return details, errors.New("Can't find video")
+	}
+
+	for _, language := range []string{"de", "fr"} {
+		apiURL := fmt.Sprintf("https://api.arte.tv/api/player/v1/config/%s/%s?platform=ARTE_NEXT", language, videoID)
+		resp, err := http.Get(apiURL)
+		if err != nil {
+			return details, err
+		}
+
+		defer resp.Body.Close()
+
+		type responseMediaVersion struct {
+			URL string `json:"url"`
+		}
+		type responseVideoJSONPlayer struct {
+			Title       string                          `json:"VTI"`
+			Description string                          `json:"VDE"`
+			VSR         map[string]responseMediaVersion `json:"VSR"`
+		}
+		type responseStruct struct {
+			Details responseVideoJSONPlayer `json:"videoJsonPlayer"`
+		}
+		var response responseStruct
+		json.NewDecoder(resp.Body).Decode(&response)
+
+		details.Title[language] = response.Details.Title
+		details.Description[language] = response.Details.Description
+		details.URL[language] = response.Details.VSR["HTTPS_SQ_1"].URL
+		details.Languages = append(details.Languages, language)
+	}
+
+	return details, nil
 }
 
 func main() {
