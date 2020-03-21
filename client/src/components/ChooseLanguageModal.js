82208ec5b9dca353cbc3150c09c08c1aa99cca1d
Lucas Hild <contact@lucas-hild.de>
2020-03-21 15:32:55 +0100

    Hide unavailable languages

--- client/src/components/ChooseLanguageModal.js
+++ client/src/components/ChooseLanguageModal.js
@@ -13,21 +13,30 @@ class ChooseLanguage extends Component {
         this.props.setLanguage(language)
     }
 
-    render() {
-        let frenchStyle = this.props.language === 'fr' ? styles.active : {}
-        let germanStyle = this.props.language === 'de' ? styles.active : {}
+    getStyle = language => {
+        if (this.props.videoDetails === null) {
+            return {}
+        } else if (this.props.language === language) {
+            return styles.active
+        } else if (!this.props.videoDetails.languages.includes(language)) {
+            return styles.hide
+        }
+        return {}
+    }
 
+    render() {
         return (
             <Modal title="Choose Language" show={this.props.showLanguageModal} onClose={() => this.props.setShowLanguageModal(false)}>
                 <div className={styles.container}>
-                    <svg onClick={this.handleClick.bind(this, 'fr')} className={frenchStyle} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3 2">
+
+                    <svg onClick={this.handleClick.bind(this, 'fr')} className={this.getStyle('fr')} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3 2">
                         <desc>Flag of France</desc>
                         <rect width="1" height="3" y="0" x="0" fill="#ED2939" />
                         <rect width="2" height="3" y="0" x="1" fill="#fff" />
                         <rect width="3" height="3" y="0" x="2" fill="#002395" />
                     </svg>
 
-                    <svg onClick={this.handleClick.bind(this, 'de')} className={germanStyle} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 3">
+                    <svg onClick={this.handleClick.bind(this, 'de')} className={this.getStyle('de')} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 3">
                         <desc>Flag of Germany</desc>
                         <rect id="black_stripe" width="5" height="3" y="0" x="0" fill="#000" />
                         <rect id="red_stripe" width="5" height="2" y="1" x="0" fill="#D00" />
@@ -42,6 +51,7 @@ class ChooseLanguage extends Component {
 
 const mapStateToProps = state => ({
     language: state.video.language,
+    videoDetails: state.video.details,
     showLanguageModal: state.main.showLanguageModal,
 })
 
