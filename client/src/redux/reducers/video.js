35091a9e317240f20f9c152e4937b2e6e84e3887
Lucas Hild <contact@lucas-hild.de>
2020-03-24 17:05:55 +0100

    Fix playback state after jump to time

--- client/src/redux/reducers/video.js
+++ client/src/redux/reducers/video.js
@@ -48,8 +48,10 @@ export default function (state = initialState, action) {
             }
 
         case SET_JUMP_TO_TIME_LAST_UPDATE:
+            let playbackState = state.playbackState == 'waiting' ? 'playing' : state.playbackState
             return {
                 ...state,
+                playbackState,
                 currentTime: action.payload,
                 jumpToTimeLastUpdate: action.payload
             }
