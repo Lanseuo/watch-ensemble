84d40ea172c94827d11743d8bfc295d317d550b4
Lucas Hild <contact@lucas-hild.de>
2020-03-24 17:06:24 +0100

    Show raw incoming websocket message instead of struct

--- internal/server/utils.go
+++ internal/server/utils.go
@@ -1,5 +1,12 @@
 package server
 
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/gorilla/websocket"
+)
+
 func findMax(a []int) (max int) {
 	max = a[0]
 	for _, value := range a {
@@ -8,4 +15,18 @@ func findMax(a []int) (max int) {
 		}
 	}
 	return max
+}
+
+func parseWebsocketJSON(ws *websocket.Conn, v interface{}) (string, error) {
+	_, reader, err := ws.NextReader()
+	if err != nil {
+		return "", err
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(reader)
+	msgString := buf.String()
+
+	err = json.Unmarshal([]byte(msgString), v)
+	return msgString, err
 }
