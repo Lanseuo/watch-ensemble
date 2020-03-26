bf464e3fc66fd7af2d4a78ce674b93a1ec1bcd73
Lucas Hild <contact@lucas-hild.de>
2020-03-26 18:32:56 +0100

    Add random background color to client list

--- client/src/components/Clients.js
+++ client/src/components/Clients.js
@@ -8,13 +8,18 @@ class Clients extends Component {
         return name.charAt(0)
     }
 
+    getRandomColor = () => {
+        let colors = ['#1565c0', '#ff1744', '#388e3c', '#9c27b0', '#4dd0e1', '#ffc107', '#f4511e', '#afb42b', '#0091ea', '#827717']
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
+
     render() {
         return (
             <div className={styles.container}>
                 <p>Clients</p>
                 {this.props.clients.map((client, index) => (
                     <div className={styles.client} key={index}>
-                        <div className={styles.clientInner}>
+                        <div className={styles.clientInner} style={{ background: this.getRandomColor() }}>
                             <p>
                                 {this.getFirstLetter(client)}
                                 <span className={styles.tooltip}>{client}</span>
