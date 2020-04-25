fcf7b81c85d337ae1b2d7b0548fb1a0da04e89dd
Lucas Hild <contact@lucas-hild.de>
2020-04-25 10:46:04 +0200

    Add nav bar and button to home page

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,27 +1,28 @@
 import React, { Component } from 'react'
+import { withRouter, RouteComponentProps } from 'react-router'
 
-interface Props {
-}
+import styles from './Home.module.css'
+import Logo from '../../logo.svg';
 
-interface State {
-}
+interface Props extends RouteComponentProps { }
+
+interface State { }
 
 class Home extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props)
-        this.state = {
-
-        }
-    }
-
     render() {
-
         return (
             <div>
-
+                <nav className={styles.nav}>
+                    <div className={styles.navInner}>
+                        <img className={styles.logo} src={Logo} alt="WatchEnsemble Logo" />
+                    </div>
+                    <div className="button-wrapper">
+                        <button onClick={() => this.props.history.push('/room')}>Join Room</button>
+                    </div>
+                </nav>
             </div>
         )
     }
 }
 
-export default Home
+export default withRouter(Home)
